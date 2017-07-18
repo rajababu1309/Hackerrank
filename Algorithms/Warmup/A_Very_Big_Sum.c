c5d8d473c8532d0312cf44fb9e80cf3f125f6789
rajababu1309 <rajababu1309@gmail.com>
2017-07-18 22:21:33 +0530

    Create A_Very_Big_Sum.c

---
+++ Algorithms/Warmup/A_Very_Big_Sum.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int n; 
+    scanf("%d",&n);
+    long int arr[n],sum=0;
+    for(int arr_i = 0; arr_i < n; arr_i++){
+       scanf("%ld",&arr[arr_i]);
+        sum+=arr[arr_i];
+    }
+    printf("%ld",sum);
+    return 0;
+}
